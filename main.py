ff9cb798112b0e6685f7d10f612f72c5b07f9ffe
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-11-12 20:00:50 +0800

    randomly select types

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import time
 hexadecimal = '0123456789abcdef'
 def hexadecimal(size: int):
-    return ''.join(random.choices(hexadecimal,k=size))
+    return ''.join(random.choices('0123456789abcdef',k=size))
 def mk():
     return (
         random.randint(1,80000) >= random.randint(75000,80000)
@@ -29,9 +29,14 @@ def type1():
 def type2():
     branches = 1
     step = 0
+    front = ' '*16
     while True:
-        print('* ',hexadecimal(7))
-        for i in range(3):
-            print('|')
+        print(front,'* ',hexadecimal(7))
+        for _ in range(3):
+            print(front,'|')
+
+TYPES = [type1, type2]
+
 if __name__ == '__main__':
-    type1()
+    chosenType = random.choices(TYPES,k=1)[0]
+    chosenType()
