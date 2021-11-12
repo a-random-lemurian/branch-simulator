cc04e0d8f1a9850b957d098b02d1b51844800c46
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-11-12 19:52:58 +0800

    implement branch simulation into a function

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ def rm():
     return (
         random.randint(1,80000) >= random.randint(75000,80000)
            )
-branches = 1
-step = 0
-while True:
-    if mk():
-        output = '|'*branches+'\\'
-        branches += 1
-    elif ( branches != 1 and rm() ):
-        output = '|'*(branches-1)+'/'
-        branches -= 1
-    else:
-        output = '|'*branches
-    print(f'{output:<60} {branches:>9} {step}')
-    step += 1
-    time.sleep(0.001)
+def type1():
+    branches = 1
+    step = 0
+    while True:
+        if mk():
+            output = '|'*branches+'\\'
+            branches += 1
+        elif ( branches != 1 and rm() ):
+            output = '|'*(branches-1)+'/'
+            branches -= 1
+        else:
+            output = '|'*branches
+        print(f'{branches:<6} {step:<8} {output:<60}')
+        step += 1
+        time.sleep(0.001)
+
+if __name__ == '__main__':
+    type1()
