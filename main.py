d684795ee2a727aa3a7b90d466d358aa0d7b1ac1
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-11-12 19:56:28 +0800

    implement a type 2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 import time
+hexadecimal = '0123456789abcdef'
+def hexadecimal(size: int):
+    return ''.join(random.choices(hexadecimal,k=size))
 def mk():
     return (
         random.randint(1,80000) >= random.randint(75000,80000)
@@ -23,6 +26,12 @@ def type1():
         print(f'{branches:<6} {step:<8} {output:<60}')
         step += 1
         time.sleep(0.001)
-
+def type2():
+    branches = 1
+    step = 0
+    while True:
+        print('* ',hexadecimal(7))
+        for i in range(3):
+            print('|')
 if __name__ == '__main__':
     type1()
