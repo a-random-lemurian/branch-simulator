c9319b1029ffba0c4c73bf4bb183c20384a7c975
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-11-13 20:16:57 +0800

    change import aliases to prevent name conficts

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 
-from type1 import branchsim as type1
-from type2 import branchsim as type2
-from type3 import branchsim as type3
+from type1 import branchsim as type1_
+from type2 import branchsim as type2_
+from type3 import branchsim as type3_
 
-TYPES = [type1, type2, type3]
+TYPES = [type1_, type2_, type3_]
 
 if __name__ == '__main__':
     chosenType = random.choices(TYPES,k=1)[0]
