983fd4ba6dce056b7faeec8234adc369553fa8e3
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-11-13 20:13:00 +0800

    comment out unused variables in type2

--- type2.py
+++ type2.py
@@ -1,7 +1,7 @@
 from main import hexadecimal
 def type2():
-    branches = 1
-    step = 0
+    #branches = 1
+    #step = 0
     front = ' '*16
     while True:
         output = front+'* '+hexadecimal(7)
